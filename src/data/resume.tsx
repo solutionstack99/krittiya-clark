c0434234c5fe958c1e3686daf2f11da3ccaa7f32
solutionstack99 <solution.stack99@gmail.com>
2025-03-20 12:45:45 -0400

    Add a type definition for the work entries.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,6 +1,19 @@
 import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
+interface WorkEntry {
+  company: string;
+  href: string;
+  badges: readonly string[];
+  location: string;
+  title: string;
+  icon: string;
+  logoUrl: string;
+  start: string;
+  end: string;
+  description: string;
+}
+
 export const DATA = {
   name: "Kritiya Clark",
   initials: "DV",
@@ -35,21 +48,18 @@ export const DATA = {
         name: "GitHub",
         url: "https://github.com/solutionstack99",
         icon: Icons.github,
-
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
         url: "https://www.linkedin.com/in/krittiya-clark-software-engineer",
         icon: Icons.linkedin,
-
         navbar: true,
       },
       X: {
         name: "X",
         url: "https://x.com/krittiyaClark",
         icon: Icons.x,
-
         navbar: true,
       },
       Youtube: {
@@ -62,12 +72,10 @@ export const DATA = {
         name: "Send Email",
         url: "#",
         icon: Icons.email,
-
         navbar: false,
       },
     },
   },
-
   work: [
     {
       company: "Independent Contractor",
